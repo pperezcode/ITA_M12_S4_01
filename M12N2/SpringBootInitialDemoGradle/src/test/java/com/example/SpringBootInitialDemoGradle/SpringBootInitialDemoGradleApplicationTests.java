ec806c3f82d0fbdab7941de4028d7b8f0055bab1
pperezcode <perezgallego.patricia@gmail.com>
2021-10-14 16:07:09 +0200

    M12N2

---
+++ M12N2/SpringBootInitialDemoGradle/src/test/java/com/example/SpringBootInitialDemoGradle/SpringBootInitialDemoGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootInitialDemoGradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootInitialDemoGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
